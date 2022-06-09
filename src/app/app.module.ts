9c95d0c30f6d672dfc563e85258c85fbe5f3b6ed
Fernando Chicaiza <washopilot@yahoo.com>
2022-06-09 17:09:09 -0500

    instalando y configurando ngx-sockets.io

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Import Sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 
+// SocketIOConfig
+const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SocketIoModule.forRoot(config)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
